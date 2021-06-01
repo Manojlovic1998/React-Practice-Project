f52610c2696f71cc47506383a719c510c13f79b5
Manojlovic1998 <manojlovic911@hotmail.com>
2021-06-02 01:03:59 +0200

    Create UI/Card component and Add it to AddUser component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,3 +1,5 @@
+import Card from "../UI/Card";
+
 
 const AddUser = props => {
 
@@ -7,13 +9,15 @@ const AddUser = props => {
     };
 
     return (
-        <form onSubmit={userSubmitHandler}>
-            <label htmlFor="username">Username</label>
-            <input type="username" id="username"/>
-            <label htmlFor="age">Age <small>(Years)</small></label>
-            <input type="number" id="age"/>
-            <button type="submit">Add User</button>
-        </form>
+        <Card>
+            <form onSubmit={userSubmitHandler}>
+                <label htmlFor="username">Username</label>
+                <input type="username" id="username"/>
+                <label htmlFor="age">Age <small>(Years)</small></label>
+                <input type="number" id="age"/>
+                <button type="submit">Add User</button>
+            </form>
+        </Card>
     );
 };
 
