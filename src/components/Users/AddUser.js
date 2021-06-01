3b02c4e3f1541c9cfb1145d80e9a58780351cb0a
Manojlovic1998 <manojlovic911@hotmail.com>
2021-06-02 00:52:25 +0200

    Create AddUser component and Add it to the App

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,21 @@
+
+const AddUser = props => {
+
+    const userSubmitHandler = event => {
+
+        event.preventDefault();
+    };
+
+    return (
+        <form onSubmit={userSubmitHandler}>
+            <label htmlFor="username">Username</label>
+            <input type="username" id="username"/>
+            <label htmlFor="age">Age <small>(Years)</small></label>
+            <input type="number" id="age"/>
+            <button type="submit"></button>
+        </form>
+    );
+};
+
+
+export default AddUser;
