de7e2c15fe187a8cc089f7f84e2931c4a9dcac6a
Manojlovic1998 <manojlovic911@hotmail.com>
2021-06-02 06:59:43 +0200

    Add validation logic to form inputs, no regex just low level react states and js methods

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,22 +1,49 @@
+import {useState} from 'react';
 import Card from "../UI/Card";
 import Button from "../UI/Button";
-import style from "./AddUser.module.css";
+import styles from "./AddUser.module.css";
 
 const AddUser = props => {
-
+    const [isValidName, setIsValidName] = useState(true);
+    const [isValidAge, setIsValidAge] = useState(true);
+    const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
     const userSubmitHandler = event => {
-
         event.preventDefault();
+        if (enteredUsername.trim().length === 0) {
+            setIsValidName(false);
+            return;
+        }
+        if (enteredAge.trim().length === 0) {
+            setIsValidAge(false);
+            return;
+        }
+    };
+
+    const userAgeChangeHandler = event => {
+        if (enteredAge.trim().length > 0) {
+            setIsValidAge(true);
+        }
+        setEnteredAge(event.target.value);
+    };
+
+    const usernameChangeHanlder = event => {
+        if (enteredUsername.trim().length) {
+            setIsValidName(true);
+        }
+        setEnteredUsername(event.target.value);
     };
 
     return (
-        <Card className={style.input}>
+        <Card className={styles.input}>
             <form onSubmit={userSubmitHandler}>
                 <label htmlFor="username">Username</label>
-                <input type="username" id="username"/>
+                <input className={!isValidName ? styles.incorrect : ''} value={enteredUsername}
+                       onChange={usernameChangeHanlder} type="username" id="username"/>
                 <label htmlFor="age">Age <small>(Years)</small></label>
-                <input type="number" id="age"/>
-                <Button>Add User</Button>
+                <input className={!isValidAge ? styles.incorrect : ''} value={enteredAge}
+                       onChange={userAgeChangeHandler} type="number" id="age"/>
+                <Button type="submit">Add User</Button>
             </form>
         </Card>
     );
